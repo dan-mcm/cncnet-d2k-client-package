20b89eba1052492479191307fce85860b4681209
dan-mcm <daniel40392@gmail.com>
2025-05-05 21:34:39 +0100

    removing gruntmods Game client dir, unnecessary for now

--- ClientFiles/Game/CnCNet5/LAN/NoAdmin.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-REG ADD "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /f /v "%~dp0CnCNet.LAN.IrcD.Server.exe" /t REG_SZ /d RUNASINVOKER
--- ClientFiles/Game/CnCNet5/Language/readme.txt
+++
@@ -1,1 +0,0 @@
-Place client translations in here, the client will automatically find and list them in the settings dialog
